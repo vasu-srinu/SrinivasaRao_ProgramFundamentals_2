e648bc6aa772a792677aaf4c7ebe6e4228883c82
vasu-srinu <141130463+vasu-srinu@users.noreply.github.com>
2023-08-04 14:50:43 +0530

    Add files via upload

---
+++ GrandedCodingAssignment1/src/com/gradedproject/q1/AdminDepartment.java
@@ -0,0 +1,22 @@
+package com.gradedproject.q1;
+
+public class AdminDepartment extends SuperDepartment{
+
+	public String deparmentName() {
+		// TODO Auto-generated method stub
+		return " Welcome to Admin Department";
+	}
+
+	public String isTodayAHoliday() {
+		// TODO Auto-generated method stub
+		return "Today is not holiday";
+	}
+
+	public String getWorkDeadline() {
+		// TODO Auto-generated method stub
+		return "Complete by EOD ";
+	}
+	public String getTodaysWork() {
+		return "Commplete your documents submission";
+	}
+}
